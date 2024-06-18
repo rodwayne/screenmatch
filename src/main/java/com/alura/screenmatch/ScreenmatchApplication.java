fa80eaee9396dbbd12ec39bb2f50382ced5107cf
rodwayne <ricardo260898@gmail.com>
2024-06-18 11:23:23 -0500

    Model episodes

--- src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.screenmatch.model.DatosEpisodio;
 import com.alura.screenmatch.model.DatosSerie;
 import com.alura.screenmatch.service.ConsumoAPI;
 import com.alura.screenmatch.service.ConvierteDatos;
 
 @SpringBootApplication
-public class ScreenmatchApplication  implements CommandLineRunner{
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
@@ -18,11 +19,11 @@ public class ScreenmatchApplication  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=f876605f");
-		// System.out.println(json);
 		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
+		var json = consumoAPI
+				.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=f876605f");
+		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodios);
 	}
 
 }
