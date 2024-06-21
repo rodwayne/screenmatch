f70aab83f2824fe32df8763687e68f4dce2c2c09
rodwayne <ricardo260898@gmail.com>
2024-06-21 11:06:41 -0500

    Add full fledged Serie clkass

--- src/main/java/com/alura/screenmatch/model/Serie.java
+++ src/main/java/com/alura/screenmatch/model/Serie.java
@@ -1,5 +1,7 @@
 package com.alura.screenmatch.model;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
     private Categoria genero;
@@ -8,4 +10,79 @@ public class Serie {
     private String poster;
     private Integer totalDeTemporadas;
     private Double evaluacion;
+
+    public Serie(DatosSerie datosSerie) {
+        this.titulo = datosSerie.titulo();
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
+        this.actores = datosSerie.actores();
+        this.sinopsis = datosSerie.sinopsis();
+        this.poster = datosSerie.poster();
+        this.totalDeTemporadas = datosSerie.totalDeTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Integer getTotalDeTemporadas() {
+        return totalDeTemporadas;
+    }
+
+    public void setTotalDeTemporadas(Integer totalDeTemporadas) {
+        this.totalDeTemporadas = totalDeTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo=" + titulo + ", genero=" + genero + ", actores=" + actores + ", sinopsis=" + sinopsis
+                + ", poster=" + poster + ", totalDeTemporadas=" + totalDeTemporadas + ", evaluacion=" + evaluacion
+                + "]";
+    }
+
+    
 }
